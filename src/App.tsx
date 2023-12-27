d344d65d7aca14faddb547fd1bc477b29b168f7e
YanCerqueira <yansilvacerqueira@outlook.com>
2023-12-27 19:19:42 -0300

    feat: base config style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './assets/styles/global';
+
+import defaultTheme from './assets/styles/themes/default';
+
 export const App = () => {
-  return <>xereka</>;
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <p>Teste</p>
+      </ThemeProvider>
+    </>
+  );
 };
