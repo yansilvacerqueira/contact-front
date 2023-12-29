dae620393f739a29b31d4ffb67bcf29e42e92aa5
YanCerqueira <yansilvacerqueira@outlook.com>
2023-12-28 22:02:18 -0300

    feat: add input and inputSelect

---
+++ src/components/InputSelect/InputSelect.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  border-radius: 8px;
+  outline: none;
+  paddgin: 0 16px;
+  transition: border-color 0.2s ease-in;
+  &:focus {
+    border-color: 2px solid ${({ theme }) => theme.primary.main};
+  }
+`;
